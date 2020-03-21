238f4314c808aa8bd7a5ecf317e6fab616d7fe29
Brayan Muniz <brayanmuniz2636@gmail.com>
2020-03-21 11:29:20 -0700

    I got bored

--- src/components/StockCard.vue
+++ src/components/StockCard.vue
@@ -12,7 +12,7 @@
     >
       {{ stock.name }}
       <b-card-text>Price: {{ stock.value }}</b-card-text>
-      <b-button variant="success" @click="boughtStock(stock)">Buy</b-button>
+      <b-button variant="success" @click="buyStock(stock, 1)">Buy</b-button>
       <b-row class="my-1" v-for="type in types" :key="type">
         <b-col sm="3">
           <label :for="`type-${type}`">
@@ -31,11 +31,16 @@
 <script lang="ts">
 /* eslint-disable */
 import Vue from "vue";
+import { newStockTransaction } from "../store";
+interface stock {
+  name: string;
+  value: number;
+}
 export default Vue.extend({
   data() {
     return {
       stocks: [],
-      bought: null, // Dev Note:  At least initialize with empty [] to get functionality
+      bought: [], // Dev Note:  At least initialize with empty [] to get functionality
       toBuy: 1,
       types: ["number"] // Dev Note:  the heck is this ?
     };
@@ -45,40 +50,20 @@ export default Vue.extend({
     stockGetter() {
       this.stocks = this.$store.getters.getStocks;
     },
-
     // Future problem: How to organize stock if bought the same
-    boughtStock(stock) {
-      console.log(`User bought ${stock}`);
-      // Todo: add interface or class
-      let bought = Number(this.toBuy.data);
-      let boughtStock = {
+    buyStock(stock: stock, amount: number) {
+      console.log(stock);
+      let formatedTr: newStockTransaction = {
         stockName: stock.name,
-        stockValue: stock.value,
-        stockQuantity: bought
+        stockData: {
+          priceAtTransaction: stock.value,
+          amount,
+          time: new Date()
+        },
+        buy: true
       };
-      let userStocks = this.$store.getters.getUserStocks;
-      for (let i = 0; i < userStocks.length; i++) {
-        console.log(userStocks.name);
-      }
-      userStocks.forEach((element, index) => {
-        // console.log(element, index);
-        console.log(element, boughtStock.stockName);
-        console.log(element.name == boughtStock.stockName);
-        if (element.name == boughtStock.stockName) {
-          console.log("Bought More Stock");
-          this.$store.commit("buyMoreStock", boughtStock);
-        } else {
-          console.log("Bought New Stock");
-          this.$store.commit("buyNewStock", boughtStock);
-        }
-      });
-      // Dev Comment: I dont like how you have to type in as a string in the commit, leads to a lot of mistakes.
-      // I would make a mutation schema and export into this file and call it  store.schema.stockBuy
-      // which will return string of stockBuy just to make less mistakes and add more functionality
-      // getUserStocks getter does not exist in the store
-      // That is why it returned undefined
-      console.log(this.$store.getters.getUserStocks); // this mistake would have been avoided with typescript
-      // console.log(this.$store.getters.userStocksGetter); // the user now has stock of whatever HE just boght
+      this.$store.dispatch("buyStock", formatedTr);
+      console.log("Current stocks", this.$store.getters.getUserStocks);
     }
   },
   async beforeMount() {
--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import Vuex from "vuex";
-
 Vue.use(Vuex);
 
 export default new Vuex.Store({
@@ -8,59 +7,102 @@ export default new Vuex.Store({
     stocks: [
       {
         name: "BMW",
-        value: 200 + "$",
-        stockQuantity: 100 // ? Dev Note: why cant this just be a string of "200$" or a number of 200.
+        value: 200,
+        stockQuantity: 100
       },
       {
         name: "Google",
-        value: 150 + "$",
+        value: 150,
         stockQuantity: 599
       },
       {
         name: "Apple",
-        value: 1000 + "$",
+        value: 1000,
         stockQuantity: 1233
       },
       {
         name: "Twitter",
-        value: 100 + "$",
+        value: 100,
         stockQuantity: 100
       }
     ],
-    portfolio: [
-      {
-        funds: 10000 + "$" // Could add this to portfolio class
-      }
-    ],
+    portfolio: {
+      funds: 10000,
+      myStocks: Array<userStock>()
+    }
   },
+
   getters: {
-    // I would rename to getTotalFunds
-    totalFunds: state => {
+    getTotalFunds: state => {
       return state.portfolio[0].funds;
     },
     getStocks: state => {
       return state.stocks;
     },
     getUserStocks: state => {
-      return state.portfolio;
+      return state.portfolio.myStocks;
+    },
+    ownStock: state => (stockName: string): boolean => {
+      let doIOwnStock = false;
+      state.portfolio.myStocks.forEach(stock => {
+        if (stock.name === stockName) {
+          doIOwnStock = true;
+        }
+      });
+      return doIOwnStock;
     }
   },
   mutations: {
-    buyNewStock(state, newStock) {
-      state.portfolio.push(newStock);
-
-    },
-    buyMoreStock(state, payload) {
-      console.log(state, payload)
-      state.portfolio.forEach((element, index) => {
-        console.log(element, index)
-        if (element[index].name == payload.name) {
-          element[index].stockQuantity = payload.stockQuantity
-        }
-      });
-      // let index = 0
-      // state.portfolio
+    updateStocksBuy(state, data: newStockTransaction) {
+      let newStockPurchaseData: stockTransactionData = {
+        priceAtTransaction: data.stockData.priceAtTransaction,
+        amount: data.stockData.amount,
+        time: data.stockData.time
+      };
+      if (data.alreadyHaveStock) {
+        state.portfolio.myStocks.forEach(stock => {
+          if (stock.name === data.stockName) {
+            stock.stocksOwned.push(newStockPurchaseData);
+          }
+        });
+      } else {
+        let newStock: userStock = {
+          name: data.stockName,
+          stocksOwned: [newStockPurchaseData]
+        };
+        state.portfolio.myStocks.push(newStock);
+      }
+      console.log("Update Stock with a BUY");
     }
+    // updateStocksSell(state, data: newStockTransaction) {}
   },
-  actions: {}
+  actions: {
+    buyStock({ commit, getters }, transactionData: newStockTransaction) {
+      if (getters.ownStock(transactionData.stockName)) {
+        transactionData.alreadyHaveStock = true;
+        commit("updateStocksBuy", transactionData);
+      } else {
+        commit("updateStocksBuy", transactionData);
+      }
+    }
+    // sellStock({ commit, getters }, transactionData: newStockTransaction) {}
+  }
 });
+
+export interface newStockTransaction {
+  stockName: string;
+  stockData: stockTransactionData;
+  buy: boolean; // if false it is sell
+  alreadyHaveStock?: boolean;
+}
+
+interface stockTransactionData {
+  priceAtTransaction: number;
+  amount: number;
+  time: Date;
+}
+
+interface userStock {
+  name: string;
+  stocksOwned: Array<stockTransactionData>;
+}
