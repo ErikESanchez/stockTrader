deb86fb7507e811ce61e901eefb839cd781e7f0e
Brayan Muniz <brayanmuniz2636@gmail.com>
2020-04-10 10:25:41 -0700

    added new route npm install popper.js

---
+++ src/views/singleStock.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Load the real data for a single stock</h1>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "singleStock",
+  props: {
+    stockData: Object
+  }
+});
+</script>
+<style lang="sass" scoped>
+
+</style>
