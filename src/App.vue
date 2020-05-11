7c8af12c0aad913ad2fad6ee86dc80172bb1f7b0
Brayan Muniz <brayanmuniz2636@gmail.com>
2020-05-11 10:35:02 -0700

    deleted stockData.json

--- src/App.vue
+++ src/App.vue
@@ -8,20 +8,15 @@
 <script lang="ts">
 import Navbar from "./components/Navbar.vue";
 import { Market } from "@/Classes/Market";
-import marketData from "@/stockData.json";
 import { Stock } from "./Classes/Stock";
+
 export default {
   name: "App",
   components: {
     Navbar
   },
-  data() {
-    return {};
-  },
-  created() {
-    console.log(this.$route.params.id);
-    this.$store.dispatch("fetchUser");
-    this.funds = this.$store.getters.totalFunds;
+  async created() {
+    await this.$store.dispatch("fetchUser");
   }
 };
 </script>
