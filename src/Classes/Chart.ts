220751db5a0bb4df6cd53b435a6e13815d516f56
Erik E Sanchez <eriksanchez7247@gmail.com>
2020-06-13 11:41:28 -0500

    fixed render issue

--- src/Classes/Chart.ts
+++ src/Classes/Chart.ts
@@ -1,32 +1,34 @@
 export class Chart {
-    async renderChart(chartData: any, symbol: string) {
-        let dataCollection: chartData = {
-            labels: Array(),
-            datasets: [
-                {
-                    // * Make the stock symbols dynamic
-                    label: symbol,
-                    data: Array(),
-                    backgroundColor: ["rgb(255, 99, 132)"]
-                }
-            ]
-        };
-        Object.keys(chartData[0]).forEach((stockDate: string) => {
-            dataCollection.datasets[0].data.push(chartData[0][stockDate]["4. close"]);
-            dataCollection.labels.push(stockDate);
-        });
-        return dataCollection
-    }
-    chartOptions(): any {
-        return {
-            responsive: true,
-            // Keep it false so it stays as a rectangle
-            maintainAspectRatio: false
-        };
-    }
+  async renderChart(chartData: any, symbol: string) {
+    let dataCollection: chartData = {
+      labels: Array(),
+      datasets: [
+        {
+          // * Make the stock symbols dynamic
+          label: symbol,
+          data: Array(),
+          backgroundColor: ["rgb(255, 99, 132)"],
+        },
+      ],
+    };
+    console.log("Bruh");
+    console.log("bruh", dataCollection);
+    Object.keys(chartData).forEach((stockDate: string) => {
+      dataCollection.datasets[0].data.push(chartData[stockDate]["4. close"]);
+      dataCollection.labels.push(stockDate);
+    });
+    return dataCollection;
+  }
+  chartOptions(): any {
+    return {
+      responsive: true,
+      // Keep it false so it stays as a rectangle
+      maintainAspectRatio: false,
+    };
+  }
 }
 
 export interface chartData {
-    labels: Array<any>;
-    datasets: Array<any>;
+  labels: Array<any>;
+  datasets: Array<any>;
 }
--- src/Classes/Portfolio.ts
+++ src/Classes/Portfolio.ts
@@ -1,120 +1,116 @@
 import { Stock } from "./Stock";
 // import { newStockTransaction } from '@/store';
 import axios from "axios";
-import { db } from "../firebase"
-import apikey from "../apikey"
+import { db } from "../firebase";
+import { apikey } from "../apikey";
 import moment from "moment";
 
-// * Need to make the logic for the user stroring stocks i think, 
+// * Need to make the logic for the user stroring stocks i think,
 export class Portfolio {
-    name: string;
-    ownedStocks: Array<Stock>;
-    // ? Initialize this somehow
-    stocks: any;
-    formatedStocks!: [];
-    portfolio!: {
-        funds: Array<userPriceHistory>;
-        latestUserFunds: Number;
-        myStocks: Array<userStock>;
-        historyOfTrades: Array<userStock>;
+  name: string;
+  ownedStocks: Array<Stock>;
+  // ? Initialize this somehow
+  stocks: any;
+  formatedStocks!: [];
+  portfolio!: {
+    funds: Array<userPriceHistory>;
+    latestUserFunds: Number;
+    myStocks: Array<userStock>;
+    historyOfTrades: Array<userStock>;
+  };
+  marketDataUrl = "https://www.alphavantage.co/query";
+
+  //   portfolioWorth: number;
+  constructor(portfolioName: string, initialStocks: Array<Stock>) {
+    this.name = portfolioName;
+    this.ownedStocks = initialStocks;
+    this.stocks = Array();
+    // this.portfolioWorth;
+  }
+  addDataToStock(newStock: any) {
+    this.stocks.push(newStock);
+  }
+  buyStock(transactionData: newStockTransaction) {
+    if (this.ownStock(transactionData.stockName)) {
+      transactionData.alreadyHaveStock = true;
+      this.updateStocksBuy(transactionData);
+    } else {
+      this.updateStocksBuy(transactionData);
+    }
+  }
+  ownStock(stockName: string) {
+    let doIOwnStock = false;
+    this.portfolio.myStocks.forEach((stock) => {
+      if (stock.name === stockName) {
+        doIOwnStock = true;
+      }
+    });
+    return doIOwnStock;
+  }
+  updateStocksBuy(data: newStockTransaction) {
+    let newStockPurchaseData: stockTransactionData = {
+      priceAtTransaction: data.stockData.priceAtTransaction,
+      amount: data.stockData.amount,
+      time: data.stockData.time,
     };
-    marketDataUrl = "https://www.alphavantage.co/query";
-
-    //   portfolioWorth: number;
-    constructor(portfolioName: string, initialStocks: Array<Stock>) {
-        this.name = portfolioName;
-        this.ownedStocks = initialStocks;
-        this.stocks = Array();
-        // this.portfolioWorth;
+    if (data.alreadyHaveStock) {
+      this.portfolio.myStocks.forEach((stock) => {
+        stock.stocksOwned.push(newStockPurchaseData);
+      });
+    } else {
+      let newStock: userStock = {
+        name: data.stockName,
+        stocksOwned: [newStockPurchaseData],
+      };
+      this.portfolio.myStocks.push(newStock);
     }
-    addDataToStock(newStock: any) {
-        this.stocks.push(newStock)
-    }
-    buyStock(transactionData: newStockTransaction) {
-        if (this.ownStock(transactionData.stockName)) {
-            transactionData.alreadyHaveStock = true;
-            this.updateStocksBuy(transactionData);
-        } else {
-            this.updateStocksBuy(transactionData)
-        }
-    }
-    ownStock(stockName: string) {
-        let doIOwnStock = false;
-        this.portfolio.myStocks.forEach(stock => {
-            if (stock.name === stockName) {
-                doIOwnStock = true;
-            }
-        });
-        return doIOwnStock;
-    }
-    updateStocksBuy(data: newStockTransaction) {
-        let newStockPurchaseData: stockTransactionData = {
-            priceAtTransaction: data.stockData.priceAtTransaction,
-            amount: data.stockData.amount,
-            time: data.stockData.time
-        };
-        if (data.alreadyHaveStock) {
-            this.portfolio.myStocks.forEach(stock => {
-                stock.stocksOwned.push(newStockPurchaseData);
-            });
-        } else {
-            let newStock: userStock = {
-                name: data.stockName,
-                stocksOwned: [newStockPurchaseData]
-            };
-            this.portfolio.myStocks.push(newStock);
-        }
-    }
-
+  }
 }
 
 export interface TIME_SERIES_DAILY {
-    function: "TIME_SERIES_DAILY";
-    symbol: string;
-    interval: string;
-    apikey: string;
-    outputsize?: string;
+  function: "TIME_SERIES_DAILY";
+  symbol: string;
+  interval: string;
+  apikey: string;
+  outputsize?: string;
 }
 
-
-
 export interface newStockTransaction {
-    stockName: string;
-    stockData: stockTransactionData;
-    buy: boolean; // if false it is sell
-    alreadyHaveStock?: boolean;
+  stockName: string;
+  stockData: stockTransactionData;
+  buy: boolean; // if false it is sell
+  alreadyHaveStock?: boolean;
 }
 
 interface stockTransactionData {
-    priceAtTransaction: number;
-    amount: number;
-    time: Date;
+  priceAtTransaction: number;
+  amount: number;
+  time: Date;
 }
 
 interface userStock {
-    name: string;
-    stocksOwned: Array<stockTransactionData>;
+  name: string;
+  stocksOwned: Array<stockTransactionData>;
 }
 
 export interface userPriceHistory {
-    funds: number;
-    time: Date;
+  funds: number;
+  time: Date;
 }
 
 export let storeSchema = {
-    mutations: {
-        buyNewStock: "buyNewStock",
-        buyMoreStock: "buyMoreStock"
-    }
+  mutations: {
+    buyNewStock: "buyNewStock",
+    buyMoreStock: "buyMoreStock",
+  },
 };
 
-    // Todo: learn how to type cast functions
-    // let userStocks = new Portfolio("Erik", )
-    //  ?Problem is what if he buys the same stock
-    //   public updatePortfolioWorth(): number {
-    //     let totalWorth: number = 0
-    //     this.ownedStocks.forEach(stock => {
-    //         totalWorth.
-    //     });
-    //   }
-
+// Todo: learn how to type cast functions
+// let userStocks = new Portfolio("Erik", )
+//  ?Problem is what if he buys the same stock
+//   public updatePortfolioWorth(): number {
+//     let totalWorth: number = 0
+//     this.ownedStocks.forEach(stock => {
+//         totalWorth.
+//     });
+//   }
