d296e762c65fca71b259eca8d10346d14da09e11
Erik E Sanchez <eriksanchez7247@gmail.com>
2020-06-13 10:11:06 -0500

    it doesn't re-render the same data

--- src/views/Stocks.vue
+++ src/views/Stocks.vue
@@ -28,25 +28,11 @@ export default Vue.extend({
     };
   },
   async mounted() {
-    console.log(this.checkForData());
-    if (this.checkForData() !== false) {
-      console.log("Called API");
-      let stockList: Array<string> = [
-        "AAPL",
-        "GOOGL",
-        "MSFT",
-        "AMZN",
-        "FB",
-        "INTC"
-      ];
-      await stockList.forEach(async (symbol: string) => {
-        this.$store.dispatch("getApiDaily", symbol);
-      });
-      this.getDatabaseDailyData();
-    } else {
-      console.log("Didn't call the API");
-      this.getDatabaseDailyData();
+    // console.log(this.checkForData());
+    if (this.stockData[0] === undefined) {
+      await this.getDatabaseDailyData();
     }
+    console.log("bruh");
     this.dataReady = true;
   },
   watch: {
@@ -61,25 +47,68 @@ export default Vue.extend({
   },
   computed: {
     stockData() {
-      return store.state.marketData.formatedStocks;
+      let stocks: Array<Object> = store.state.marketData.formatedStocks;
+      if (stocks[0] !== undefined && this.checkData(stocks) === false) {
+        console.log("Need to call API");
+      } else {
+        console.log("Data is new");
+      }
+      // let yesterdaysDate: string = moment(moment().subtract(1, "day")).format(
+      //   "YYYY-MM-DD"
+      // );
+      // stocks.forEach((stock: any) => {
+      // let dateOfStock: string = stock["stockData"]["lastRefreshed"];
+      // Todo: Make this account for weekends
+      // console.log(dateOfStock);
+      // if (this.checkData() !== true && yesterdaysDate !== dateOfStock)
+      // console.log("Bruh, we need new API data");
+      // });
+      return stocks;
     }
   },
   methods: {
-    checkForData() {
+    async callAPI() {
+      let stockList: Array<string> = [
+        "AAPL",
+        "GOOGL",
+        "MSFT",
+        "AMZN",
+        "FB",
+        "INTC"
+      ];
+      await stockList.forEach(async (symbol: string) => {
+        store.dispatch("getApiDaily", symbol);
+      });
+      // this.getDatabaseDailyData();
+    },
+    checkData: (stocks: Object): boolean => {
       let dayOfWeek: Object = moment().weekday();
+      let dateAndHour: string = "YYYY-MM-DD HH:mm:ss";
       let isWeekend: boolean = dayOfWeek === 6 || dayOfWeek === 0;
-      let timeInHours: string = moment().format("HH: mm: ss");
-      console.log(moment().format("HH: mm: ss"));
+      let today: string = moment().format("YYYY-MM-DD");
+      let yesterday: string = moment(moment().subtract(1, "day")).format(
+        "YYYY-MM-DD"
+      );
+      let timeInHours: string = moment(`${today} 15:00:00`, dateAndHour).format(
+        dateAndHour
+      );
+      let newData: boolean = Boolean();
+      console.log(newData);
+      // moment(moment().format(dateAndHour)).isSameOrAfter(timeInHours);
       // ! Make this check for data in db as well
-      if (isWeekend === true) {
-        console.log(
-          "Bruh, the market ain't open. Wall Street doing the dirty right now."
-        );
-        // ! Make this account for holidays, no clue how I'm going to do it but I have to
-      } else if (isWeekend === false || moment().isSameOrAfter(timeInHours)) {
-        console.log("Bruh, you need new data.");
+      if (isWeekend === false) {
+        // if(m)
+        // console.log("burh");
+        // if(moment())
+        return false;
+      } else if (isWeekend === true) {
+        // console.log("Market's closed, Wall Street is doing the dirty");
       }
-      return isWeekend;
+      // if (isWeekend === true) {
+      //   // ! Make this account for holidays, no clue how I'm going to do it but I have to
+      // } else if (isWeekend === false && moment().isSameOrAfter(timeInHours)) {
+      //   console.log("Bruh, you need new data.");
+      // }
     },
     async APIData(stock: any) {
       this.$store.dispatch("getApiDaily", stock);
